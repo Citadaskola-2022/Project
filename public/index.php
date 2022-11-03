943c7737c9eb0d87e644a71d94d1036b0f5de3e4
Roberts Rozkalns <roberts.rozkalns@me.com>
2022-11-03 15:21:05 +0200

    3rd party packages, env variables

--- public/index.php
+++ public/index.php
@@ -4,7 +4,41 @@ declare(strict_types=1);
 
 require __DIR__ . '/../bootstrap/app.php';
 
-$transaction = new \App\ServiceCosts(111);
-$transaction->process();
+use Doctrine\Inflector\InflectorFactory;
+use Ramsey\Uuid\Uuid;
 
+$transaction = new \App\ServiceCosts(111);
+//$transaction->process();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+//echo '<pre>';
+var_dump($_ENV['DB_USER']);
+var_dump($_ENV['DB_PASSWORD']);
+
+$inflector = InflectorFactory::create()->build();
+
+$name = 'comment';
+printf(
+    'vienskaitlis: %s, daudzskaitlis: %s',
+    $name, $inflector->pluralize($name)
+);
+echo '<br>';
+
+$blogPost = 'This Is MY 4w350m3 Blog post';
+printf(
+    'title: %s, url: %s',
+    $blogPost, $inflector->urlize($blogPost)
+);
+echo '<br>';
+
+
+$uuid = Uuid::uuid4();
+printf(
+    "UUID: %s\nVersion: %d\n",
+    $uuid->toString(),
+    $uuid->getFields()->getVersion()
+);
+echo '<br>';
 
