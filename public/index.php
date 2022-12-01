2ec165335d93e5a26fe0125e9e79583786795514
Roberts Rozkalns <roberts.rozkalns@me.com>
2022-12-01 12:10:37 +0200

    clean space

--- public/index.php
+++ public/index.php
@@ -4,13 +4,3 @@ declare(strict_types=1);
 
 require __DIR__ . '/../bootstrap/app.php';
 
-/** @var \App\Form\Field[] $fields */
-$fields = [
-  new \App\Form\Text('text_field'),
-  new \App\Form\Checkbox('checkbox_field'),
-  new \App\Form\Radio('radio_field'),
-];
-
-foreach ($fields as $field) {
-    echo $field->render();
-}
