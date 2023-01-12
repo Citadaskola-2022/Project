8e3f3b68670feba8388eecaa97d84fd33297cc76
Roberts Rozkalns <roberts.rozkalns@me.com>
2023-01-12 12:39:38 +0200

    frontend basics

--- public/index.php
+++ public/index.php
@@ -1,21 +1,56 @@
-<?php
+<!doctype html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Programmēšana 2 || Frontend</title>
 
-declare(strict_types=1);
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-800 text-white h-full grid place-items-center">
 
-function summation($n) {
-    return $n > 0 ? summation($n-1) + $n : 0;
-}
+<!--    <h2>Users</h2>-->
+<!--    <ul>-->
+<!--        <li>John Doe</li>-->
+<!--        <li>Jane Doe</li>-->
+<!--        <li>Foo Bar</li>-->
+<!--    </ul>-->
 
-summation(8);
+    <div id="app">
+        <p>{{ greeting }} ({{ greeting.length }})</p>
+
+        <input
+            class="bg-gray-600 p-2"
+            v-model="greeting"
+        />
+        
+        <button
+            :class="active ? 'text-green-500' : 'text-red-500'"
+            @click="toggle"
+        >Click me</button>
+    </div>
 
 
+    <script>
+        Vue.createApp({
+            data () {
+                return {
+                    greeting: 'Hello CITADASKOLA!',
+                    active: false,
+                }
+            },
+            mounted () {
 
-require __DIR__ . '/../bootstrap/app.php';
-
-//$router = new \App\Router();
-//$router->register('/', function () {
-//   echo 'Index';
-//});
-//$router->register('/advent-of-code/1', [\App\Controllers\AdventOfCode\Day1::class, 'index']);
-//
-//echo $router->resolve($_SERVER['REQUEST_URI']);
+            },
+            methods: {
+                toggle () {
+                    this.active = !this.active;
+                }
+            }
+        }).mount('#app');
+    </script>
+</body>
+</html>
